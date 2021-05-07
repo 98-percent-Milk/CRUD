9bcb8f8fbf1a1eaa3929efad96e6b7c91de2a601
98-percent-Milk <mjantsn@my.bcit.ca>
2021-05-06 20:56:44 -0700

    Final beta version of main class

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 from quizinator import Quiz
-from flashcard import FlashCard
 from menu import Menu
 
-def main():
+def main() -> None:
+    """ Main function that runs the quizinator app """
     quiz = Quiz()
-    flashcard = FlashCard()
     menu = Menu()
 
     while True:
         menu.display_menu
         choice = menu._get_choice_input
 
-        if choice == 2:
+        if choice == 1:
+            quiz.add_flashcard()
+
+        elif choice == 2:
             print(f'All flashcards in this study set')
             quiz.view_flashcard()
 
         elif choice == 3:
-            print('\nHere are your flahscards\n', quiz.view_flashcard()) #replace with function to view all flashcards
+            print('\nHere are your flahscards\n')
+            quiz.view_flashcard() #replace with function to view all flashcards
             key = input('Which flashcard would you like to update? ')
-            flashcard.edit_flashcard(quiz.quizinator, key)
-            quiz.save_quizinator()
+            quiz.edit_flashcard(key)
 
         elif choice == 4:
             print('here are your flashcards')
-            for i in quiz.quizinator.values():
-                print(i,end =" ")
-            print()
-            flashcard.remove_flashcard(quiz.quizinator)
-            quiz.save_quizinator()
+            quiz.view_flashcard()
+            quiz.remove_flashcard()
 
         elif choice == 5:
             return False
 
-main()
+if __name__  == '__main__':
+    main()
