44aedeab3c98974a4c95434ac75cd511b81ad600
98-percent-Milk <mjantsn@my.bcit.ca>
2021-05-06 20:32:55 -0700

    integrated remove method

--- flashcard.py
+++ flashcard.py
@@ -56,3 +56,27 @@ class FlashCard:
                 definition = input('Please input new definition: ')
                 items[key] = [term, definition]
 
+    def remove_flashcard(self,quiz_list):
+        """ takes flashcard_id as input and deletes the coressponding flashcard"""
+        quiz = quiz_list
+
+        user_input  = input('Enter the flashcard id you want to remove ')
+        while user_input not in quiz.keys():
+            print('There is no flashcard', user_input)
+            user_input = input('Re-enter the term you want to remove ')
+        quiz.pop(user_input)
+        print('the flashcard', user_input ,' has been deleted')
+        dict1={}
+        for i,j in quiz.items():
+            if i.isdigit() == False:
+                dict1[i] = j
+            else:
+                if int(i) < int(user_input):
+                    dict1[i] = j
+                else:
+                    i = int(i)
+                    i-=1
+                    i = str(i)
+                    dict1[i] = j
+                    j['id'] = i
+        return dict1
