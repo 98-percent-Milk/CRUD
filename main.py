c8947a8de38cccb7d452e22a0f791b15b52b8f83
Jasmeen241 <jasnoorkaursidhu2002@gmail.com>
2021-05-07 08:40:25 +0530

    main funct

--- main.py
+++ main.py
@@ -27,8 +27,10 @@ def main():
             for i in quiz.quizinator.values():
                 print(i,end =" ")
             print()
-            flashcard.remove_flashcard(quiz.quizinator)
-            quiz.save_quizinator()
+
+            data = flashcard.remove_flashcard(quiz.quizinator)
+            flashcard.save_to_json(data)
+
 
         elif choice == 5:
             return False
