d73a40211716a77726c0ce73cddad41a43db44b8
Margad B Jantsan <71790073+98-percent-Milk@users.noreply.github.com>
2021-05-12 20:37:28 -0700

    Update flashcard.py

--- flashcard.py
+++ flashcard.py
@@ -3,7 +3,6 @@ flashcard.py Beta version of flash card for quizinator app
 Author: Team 3
 Date: April 30th
 """
-import json
 
 class FlashCard:
     """ Beta Flash Card class for quizinator app """
@@ -13,8 +12,10 @@ class FlashCard:
         self.definition = ''
         self.id = ''
 
+
     def create_flashcard(self, term:str, definition:str, unique_id:str) -> None:
         """ Create new flashcard from user input
+
         Parameter
         --------------
         term: str
@@ -36,52 +37,10 @@ class FlashCard:
         else:
             raise ValueError
 
+
     def serialize(self):
         return {
             'id': self.id,
             'term': self.term,
             'def': self.definition
         }
-    
-    def edit_flashcard(self, key):
-        """ Allows you to edit existing flashcards """
-        quiz = Quiz()
-        #for every item in the list
-        for items in quiz.quizinator:
-            #if the term matches the search term
-            if items == key:
-                #term = new_term
-                term = input('Please input new term: ')
-                #definition = new_definition
-                definition = input('Please input new definition: ')
-                items[key] = [term, definition]
-
-    def remove_flashcard(self,quiz_list):
-        """ takes flashcard_id as input and deletes the coressponding flashcard"""
-        quiz = quiz_list
-
-        user_input  = input('Enter the flashcard id you want to remove ')
-        while user_input not in quiz.keys():
-            print('There is no flashcard', user_input)
-            user_input = input('Re-enter the term you want to remove ')
-        quiz.pop(user_input)
-        print('the flashcard', user_input ,' has been deleted')
-        dict1={}
-        for i,j in quiz.items():
-            if i.isdigit() == False:
-                dict1[i] = j
-            else:
-                if int(i) < int(user_input):
-                    dict1[i] = j
-                else:
-                    i = int(i)
-                    i-=1
-                    i = str(i)
-                    dict1[i] = j
-                    j['id'] = i
-        return dict1
-
-    def save_to_json(self,dict1):
-        """ saving dict1 to json file"""
-        with open('data.json', 'w') as f:
-            json.dump(dict1,f,indent = 4)
