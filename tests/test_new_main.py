a9f22efd150980b6f5c9cfd2536a0d52f00955fb
Agnes Ko <agnes.ko68@yahoo.ca>
2021-05-26 14:13:00 -0400

    fixed tests with new imports

--- tests/test_new_main.py
+++ tests/test_new_main.py
@@ -2,44 +2,49 @@
 import pytest
 import mock
 import builtins
-from main import main
+from controllers import QuizController
 
-def test_display_menu():
+@pytest.fixture(name='main')
+def fixture_main():
+    default = QuizController()
+    return default
+
+def test_display_menu(main):
     """ Unit test for displaying the main menu """
     with mock.patch.object(builtins, 'input', side_effect=['8']):
-        main()
+        main.run()
 
-def test_menu_option_one():
+def test_menu_option_one(main):
     """ Unit test for adding new flashcard into the database """
     with mock.patch.object(builtins, 'input', side_effect=['1', 'ruby', 'Ruby', 'computer language', '8']):
-        main()
+        main.run()
 
-def test_menu_option_two():
+def test_menu_option_two(main):
     """ Unit test for viewing all the flashcards in the database """
     with mock.patch.object(builtins, 'input', side_effect=['2', '8']):
-        main()
+        main.run()
 
-def test_menu_option_three():
+def test_menu_option_three(main):
     """ Unit test for editing existing flashcard """
     with mock.patch.object(builtins, 'input', side_effect=['3','ruby', 'ruby', 'New Gem', '8']):
-        main()
+        main.run()
 
-def test_menu_option_four():
+def test_menu_option_four(main):
     """ Unit test for deleting existing flashcard """
     with mock.patch.object(builtins, 'input', side_effect=['4', '2', '8']):
-        main()
+        main.run()
 
-def test_menu_option_five():
+def test_menu_option_five(main):
     """ Unit test for searching existing flashcard """
     with mock.patch.object(builtins, 'input', side_effect=['5', 'python', '8']):
-        main()
+        main.run()
 
-def test_menu_option_six():
+def test_menu_option_six(main):
     """ Unit test for searching learning flashcard """
     with mock.patch.object(builtins, 'input', side_effect=['6', 'y', '', 'n', 'n', '', 'e', '8']):
-        main()
+        main.run()
 
-def test_menu_option_seven():
+def test_menu_option_seven(main):
     """ Unit test for practice function """
     with mock.patch.object(builtins, 'input', side_effect=['7', 'q', '8']):
-        main()
+        main.run()
